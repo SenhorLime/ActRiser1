e6a148b2a5496fb27e1df103c26cc5f3e02a1ad0
arsrc <arsrc@Desktop-Aorus>
2023-09-27 14:21:56 -0300

    Mudança o som principal do Jogo. Passa a usar o som original. Adicionado zip com efeitos de som original para posterior uso.

--- Source/Music/Music.hpp
+++ Source/Music/Music.hpp
@@ -1,7 +1,8 @@
 #include <SFML/Audio/Music.hpp>
 
 void GameMusic(sf::Music &backgroundMusic) {
-	backgroundMusic.openFromFile("Assets/Music/Monster.ogg");
+	//backgroundMusic.openFromFile("Assets/Music/Monster.ogg");
+	backgroundMusic.openFromFile("Assets/Music/15-Birth-of-the-People.ogg");
 
 	backgroundMusic.play();
 	backgroundMusic.setVolume(50);
